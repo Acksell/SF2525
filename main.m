a9196266cfbc40dda4e7a2ca756dcfb2af5fa24f
Acksell <axelen@kth.se>
2021-04-29 11:44:47 +0200

    One single file

--- main.m
+++ main.m
@@ -1,9 +1,8 @@
 
-
-Ff0_func = @(k) Ff0(k);
-
 Ns=100000;
+% Note: Ns/100 is foudn by trial and error.
 kspace = linspace(-Ns/100, Ns/100,Ns);
+
 % figure
 % plot(arrayfun(@(k) b(abs(k)), kspace)) 
 % hold on
@@ -11,15 +10,56 @@ kspace = linspace(-Ns/100, Ns/100,Ns);
 % plot(arrayfun(@(k) mu(k), kspace)) 
 % figure
 
-Ff_0 = arrayfun(Ff0_func, kspace);
+% Generate the fourier-transformed values of f0.
+Ff_0 = arrayfun(@Ff0, kspace);
 
-% plot(Ff_0);
-% plot(arrayfun(@(k) mu(k), kspace))
+% fftshift is needed to reorder the values before use of ifft.
+% ifftshift shifts them back.
 
-f0 = fftshift(ifft(ifftshift(Ff_0)));
+f0 = ifftshift(ifft(fftshift(Ff_0)));
+% Problem: Imag looks like the correct f0 function, but in reality we want
+% f0 to be a real function and for the imaginary part to simply be rounding errors.
+figure
 plot(imag(f0))
+title("Imaginary part")
 
-% hold on
-% plot(imag(f0),'bo-')
-% plot(real(f0),'ro-')
+figure
+plot(real(f0))
+title("Real part")
 
+function [res]=Fphi(k)
+    res=sign(k)*exp(1i*pi*k)*b(abs(k))/sqrt(2*pi);
+end
+
+function [res]=Ff0(k)
+    res=sqrt(E(k))*Fphi(k);
+end
+
+function [res]=nu(k)
+    res = 2*(max(0, (k-y(0)))^2 + max(0, (k-y(2)))^2 - 2*max(0, k-y(1))^2);
+end
+
+function [res]=y(r)
+    res=(cos((2-r)*pi/2)+1)/2;
+end
+
+function [res]=b(k)
+    res=sin(nu(mu(k))*pi/2);
+end
+
+function [res]=E(k)
+    res=abs(k)^(-5/3);
+end
+
+function [res]=mu(k)
+    absk=abs(k);
+    if absk<1/3
+        res=0;
+    elseif absk<2/3 && 1/3<=absk
+        res=3*(absk-1/3);
+    elseif absk<4/3 && 2/3 <= absk
+        res=1 + 3/2*(2/3-absk);
+    else
+        res=0;
+    end
+end
